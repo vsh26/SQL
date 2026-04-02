80ed90a376504a8654e6d178443569606055780c
Vishal Kapgate <vishaldk26@gmail.com>
2026-04-02 16:28:23 +0530

    Update OUTER JOIN: RIGHT JOIN

--- 009-RIGHT_JOIN.sql
+++ 009-RIGHT_JOIN.sql
@@ -18,3 +18,13 @@ SELECT C.CUSTOMER_ID, C.CUSTOMER_NAME, O.ORDER_ID, O.TOTAL_AMOUNT
     -- RIGHT JOIN can differ from INNER JOIN
     -- because Orders can exist without Customers (due to ON DELETE SET NULL)
     -- in such cases, RIGHT JOIN will include those rows with NULL customer data
+
+-- However, just to understand how table will look, we can implement query in slightly different way
+	-- (Left Table: Orders, Right Table: Customers)
+SELECT O.ORDER_ID, O.TOTAL_AMOUNT, C.CUSTOMER_ID, C.CUSTOMER_NAME
+	FROM Orders O
+    RIGHT JOIN Customers C
+    ON C.CUSTOMER_ID = O.REF_CUSTOMER_ID;
+    
+-- So, we can imply
+	-- (A LEFT JOIN B) ≡ (B RIGHT JOIN A), i.e., logically equivalent
