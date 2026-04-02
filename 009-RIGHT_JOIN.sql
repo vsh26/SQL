6dd47c6a0208a1f3226ecd6d1a65cac14a66c102
Vishal Kapgate <vishaldk26@gmail.com>
2026-04-02 16:19:06 +0530

    Add OUTER JOIN: RIGHT JOIN

---
+++ 009-RIGHT_JOIN.sql
@@ -0,0 +1,20 @@
+-- RIGHT JOIN --
+
+USE ECommerceDB;
+
+-- RIGHT JOIN:
+	-- returns a table with all data from right table and matched data from left table
+    -- if no matching row exists in left table, its columns are returned as NULL
+    
+SELECT C.CUSTOMER_ID, C.CUSTOMER_NAME, O.ORDER_ID, O.TOTAL_AMOUNT
+	FROM Customers C
+    RIGHT JOIN Orders O
+    ON C.CUSTOMER_ID = O.REF_CUSTOMER_ID;
+    
+-- In this particular DB (with current data), results may appear same as INNER JOIN
+    -- because every order currently has a matching customer
+
+-- However, in general:
+    -- RIGHT JOIN can differ from INNER JOIN
+    -- because Orders can exist without Customers (due to ON DELETE SET NULL)
+    -- in such cases, RIGHT JOIN will include those rows with NULL customer data
